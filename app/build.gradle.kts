0fb177f3c6cfe25390f9704406237519fdb897c8
ɴᴇᴋᴏ <s99095lkjjim@gmail.com>
2025-03-13 16:03:07 +0800

    namespace / remove settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
 }
 
 android {
-    namespace = "com.xiaoyv.bangumi"
+    namespace = "com.neko.bangumi"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.xiaoyv.bangumi"
+        applicationId = "com.neko.bangumi"
         minSdk = 24
         targetSdk = 34
         versionCode = 7
